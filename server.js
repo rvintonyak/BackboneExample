b1b849ad199fe3958083f92e8afcfcc11e8b8bb5
Vintonyak Roman <vintonyakroman@gmail.com>
2017-04-23 14:45:37 +0300

    Added tests

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app = connect()
   .use(connect.static('app'))
   .use('/js/lib/', connect.static('node_modules/requirejs/'))
   .use('/node_modules', connect.static('node_modules'))
+  
+  .use('/test', connect.static('test/'))
+  .use('/test', connect.static('app'))
   ;
 
 http.createServer(app).listen(8080, function() {
